64e7b9ba1b0eab6526fc98b3cc1d902de6f64139
NovikovM18 <mnovikov.5prosoftware.com>
2022-09-22 21:06:23 +0300

    deploy v.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD3bkMBrQqkZhLqs966gh9_p8oREi-VFLQ",
+    authDomain: "my-chat-25e1b.firebaseapp.com",
+    projectId: "my-chat-25e1b",
+    storageBucket: "my-chat-25e1b.appspot.com",
+    messagingSenderId: "98518208036",
+    appId: "1:98518208036:web:4fea9bbd5d57d86627d2b1"
+  }
 };

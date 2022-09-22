403e85fcc286d03e75fdfe6ad566b575120b9f3e
NovikovM18 <mnovikov.5prosoftware.com>
2022-09-22 19:38:32 +0300

    add firestore auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD3bkMBrQqkZhLqs966gh9_p8oREi-VFLQ",
+    authDomain: "my-chat-25e1b.firebaseapp.com",
+    projectId: "my-chat-25e1b",
+    storageBucket: "my-chat-25e1b.appspot.com",
+    messagingSenderId: "98518208036",
+    appId: "1:98518208036:web:4fea9bbd5d57d86627d2b1"
+  }
 };
 
 /*
